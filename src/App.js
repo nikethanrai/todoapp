dbb54c479f42aa6ce915c13856186ef03ab8c113
nikethanrai <nikethanrai58@gmail.com>
2021-07-28 20:48:06 +0530

    filter left

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 
 import './App.css';
+import Form from './components/form'
+import TodoList from './components/Todolist';
+import React, { useState, useEffect } from 'react';
+
 
 function App() {
+
+  const [inputText, setInputText] = useState('')
+  const [todos, setTodos] = useState([])
+  const [status, setStatus] = useState('all')
+  const [filteredTodos, setFilteredTodos] = useState([])
+
+  useEffect(() => {
+    filterHandler();
+  }, [todos, status]);
+
+  const filterHandler = () => {
+    switch (status) {
+      case 'completed':
+        setFilteredTodos(todos.filter(todo => todo.completed === true))
+        break
+      case 'incomplete':
+        setFilteredTodos(todos.filter(todo => todo.completed === false))
+        break
+      default:
+        setFilteredTodos(todos)
+        break
+
+
+    }
+  }
   return (
+
     <div className="App">
-      <h1>hello</h1>
+      <header><h1>Niki's TodoList</h1>
+        <Form todos={todos} filteredTodos={filteredTodos} setStatus={setStatus} inputText={inputText} setTodos={setTodos} setInputText={setInputText} />
+        <TodoList todos={todos} setTodos={setTodos} />
+
+      </header>
+
     </div>
+
   );
 }
 
